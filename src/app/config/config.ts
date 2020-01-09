c0322cf485d369588bb45e17aed48d8dee2ef485
citkcaamal <programaciondyn3.mx@sandos.com>
2020-01-09 14:27:31 -0500

    definiendo endpoints para consumir servicios desde api.net

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,1 +1,27 @@
-export const URL_SERVICIOS = 'http://localhost:3000/';
+export const URL_SERVICIOS = 'http://localhost:3000/'; // de mi servidor Express
+
+export const URL_SERVICIOS_API = 'http://172.16.4.229/ApiBodas/'; // servidor IIS
+
+export const ENDPOINTS =  {
+    hotel: {
+        getHoteles: URL_SERVICIOS_API + 'Hoteles/getHoteles',
+        getHotel: URL_SERVICIOS_API + 'Hoteles/getHotelById/',
+        saveHotel: URL_SERVICIOS_API + 'Hoteles/saveHotel',
+        updateHotel: URL_SERVICIOS_API + 'Hoteles/updateHotel/',
+        deleteteHotel: URL_SERVICIOS_API + 'Hoteles/deleteHotel/'
+    },
+    agencia : {
+        getAgencias: URL_SERVICIOS_API + 'Agencia/getAgencias',
+        getAgencia: URL_SERVICIOS_API + 'Agencia/getAgenciaById/',
+        saveAgencia: URL_SERVICIOS_API + 'Agencia/saveAgencia',
+        updateAgencia: URL_SERVICIOS_API + 'Agencia/updateAgencia/',
+        deleteAgencia: URL_SERVICIOS_API + 'Agencia/deleteAgencia/'
+    },
+    horario : {
+        gethorarios: URL_SERVICIOS_API + 'Horarios/getHorarios',
+        getHorario: URL_SERVICIOS_API + 'Horarios/getHorarioById/',
+        saveHorario: URL_SERVICIOS_API + 'Horarios/saveHorario',
+        updateHorario: URL_SERVICIOS_API + 'Horarios/updateHorario/',
+        deleteHorario: URL_SERVICIOS_API + 'Horarios/deleteHorario/'
+    }
+}
--- src/app/pages/dashboard/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+// import { ENDPOINTS } from './../../../config/config';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor() {  }
 
   ngOnInit() {
   }
