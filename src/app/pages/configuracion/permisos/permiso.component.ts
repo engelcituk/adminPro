c365a9b146b899360f02246dd08a2879388db661
citkcaamal <programaciondyn3.mx@sandos.com>
2020-02-25 17:57:39 -0500

    permisos, guardado y get permiso

--- src/app/pages/configuracion/permisos/permiso.component.ts
+++ src/app/pages/configuracion/permisos/permiso.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, NgForm } from '@angular/forms';
+import { PermisoService} from '../../../services/service.index';
+import { Permiso } from './../../../models/permiso.model';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-permiso',
@@ -7,9 +11,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PermisoComponent implements OnInit {
 
-  constructor() { }
+  formPermiso: FormGroup;
+  permiso: Permiso = new Permiso('', '', false, '');
+  idHotel: any;
+  identificador = false;
+
+  constructor(
+    public permisoService: PermisoService,
+    public router: Router,
+    public rutaActivada: ActivatedRoute
+    ) {
+  }
 
   ngOnInit() {
+    const id: any = this.rutaActivada.snapshot.paramMap.get('id');
+    if (id !== 'nuevo') {
+      this.permisoService.getPermiso(id).subscribe((respuesta: Permiso) => {
+        this.permiso = respuesta;
+        this.identificador = false;
+      });
+    }
+  }
+
+  getPermiso(id: string) {
+    this.permisoService.getPermiso(id)
+      .subscribe((permiso: Permiso) => {
+        this.permiso = permiso;
+      });
+  }
+  savePermiso(formPermiso: NgForm) {
+    if (formPermiso.invalid) {
+      return;
+    }
+    this.permisoService.savePermiso(this.permiso).subscribe(permiso => {
+      this.permiso._id = permiso._id;
+      this.router.navigate(['/permisos', this.permiso._id]);
+    });
   }
 
 }
--- src/app/pages/configuracion/permisos/permisos.component.ts
+++ src/app/pages/configuracion/permisos/permisos.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { PermisoService } from 'src/app/services/service.index';
+import { Permiso } from '../../../models/permiso.model';
+
+import Swal from 'sweetalert2';
+
+
 
 @Component({
   selector: 'app-permisos',
@@ -6,10 +12,22 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class PermisosComponent implements OnInit {
+  permisos: Permiso[] = [];
+  totalRegistros: number = 0;
+  cargando: boolean;
 
-  constructor() { }
+  constructor(public permisoService: PermisoService) {}
 
   ngOnInit() {
+    this.getPermisos();
   }
 
+  getPermisos() {
+    this.cargando = true;
+    this.permisoService.getPermisos().subscribe((respuesta: any) => {
+      this.permisos = respuesta.permisos;
+      this.totalRegistros = respuesta.total;
+      this.cargando = false;
+    });
+  }
 }
